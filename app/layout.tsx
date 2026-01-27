4a0e1f96da18369bb2c1407706910c7627ed0799
FlipVenter <flippiefliptone@gmail.com>
2026-01-27 15:51:39 +0200

    added some styling and background to the home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} overscroll-x-none`}>
+      <body className={`${inter.variable} overscroll-x-none overscroll-y-none`}>
         <Navbar />
         <main>
           {children}
