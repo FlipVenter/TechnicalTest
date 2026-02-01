492871ad09e995535b0763b7de9d4bfa23d2183a
FlipVenter <flippiefliptone@gmail.com>
2026-02-01 19:29:54 +0200

    made dropdown roll down and hide on page reload

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,18 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 import React, { useState } from "react";
 import Link from "next/link";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
 
   const navLinkClass =
     "relative text-black transition-colors hover:text-sun " +
@@ -46,6 +54,44 @@ const Navbar = () => {
             <span className="w-7 h-0.5 bg-black"></span>
             <span className="w-7 h-0.5 bg-black"></span>
           </button>
+
+          {/* Mobile dropdown */}
+          <div
+            className={`
+            sm:hidden
+            absolute top-full left-0 w-full
+            bg-white shadow-lg
+            overflow-hidden
+            transition-all duration-300 ease-out
+            ${isOpen ? "max-h-60 opacity-100 translate-y-0" : "max-h-0 opacity-0 -translate-y-2"}
+          `}
+          >
+            <div className="flex flex-col items-center gap-6 py-6">
+              <Link
+                href="/services"
+                onClick={() => setIsOpen(false)}
+                className="text-lg hover:text-sun"
+              >
+                Services
+              </Link>
+
+              <Link
+                href="/about"
+                onClick={() => setIsOpen(false)}
+                className="text-lg hover:text-sun"
+              >
+                About Us
+              </Link>
+
+              <Link
+                href="/contact"
+                onClick={() => setIsOpen(false)}
+                className="text-lg hover:text-sun"
+              >
+                Contact
+              </Link>
+            </div>
+          </div>
 
           {/* Desktop links */}
           <div
